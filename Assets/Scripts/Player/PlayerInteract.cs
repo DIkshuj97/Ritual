3b5ee072f48db1bcac5f9a7ad5490cc2ad7b8307
Irtaza Khan <irtazakhan100@gmail.com>
2022-12-30 23:21:12 +0100

    Interaction
    Added basic Interact mechanics for player and interactable object

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private float rayRange = 4;
+    [SerializeField] private Transform head;
+    [SerializeField] private GameObject handIcon;
+    [SerializeField] private LayerMask interactLayer;
+
+    void Update()
+    {
+        CastRay();
+    }
+
+    void CastRay()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        Ray ray = new Ray(head.position, head.forward);
+        bool hit = Physics.Raycast(ray, out hitInfo, rayRange, interactLayer);
+        if (hit)
+        {
+            handIcon.SetActive(true);
+            GameObject hitObject = hitInfo.transform.gameObject;
+            if (hitObject.GetComponent<IInteractable>() != null && Input.GetKeyDown(KeyCode.E))
+            {
+                hitObject.GetComponent<IInteractable>().Interact();
+            }
+        } else
+        {
+            handIcon.SetActive(false);
+        }
+    }
+}
