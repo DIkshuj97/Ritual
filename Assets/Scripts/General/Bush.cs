c97e5f4dfe1a1213cd83d91525b3cc52e65b9dee
Irtaza Khan <irtazakhan100@gmail.com>
2023-01-05 22:41:24 +0100

    Bush feature
    Bush Hiding Transparent feature added

---
+++ Assets/Scripts/General/Bush.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRend;
+    [SerializeField] private Material transparentMaterial;
+    Material originalMat;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMat = meshRend.sharedMaterial;
+    }
+    
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) SetModeTransparent();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) RevertMode();
+    }
+    private void SetModeTransparent()
+    {
+        meshRend.sharedMaterial = transparentMaterial;
+    }
+    private void RevertMode()
+    {
+        meshRend.sharedMaterial = originalMat;
+    }
+}
