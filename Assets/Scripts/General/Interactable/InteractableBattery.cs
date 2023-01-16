947196b42d8d7bbcf4cedc9007b5f3a5214f69de
Irtaza Khan <irtazakhan100@gmail.com>
2023-01-16 02:25:33 +0100

    final bug fixes

--- Assets/Scripts/General/Interactable/InteractableBattery.cs
+++ Assets/Scripts/General/Interactable/InteractableBattery.cs
@@ -6,7 +6,9 @@ public class InteractableBattery : MonoBehaviour, IInteractable
 {
     public void Interact()
     {
-        PlayerInteract.canInteractAgain = true;
+        //PlayerInteract.canInteractAgain = true;
+        var dt = GetComponent<DialogueTrigger>();
+        if (dt != null) dt.TriggerDialogue();
         GameManager.ins.fLightScript.batteries++; 
         gameObject.SetActive(false);
     }
